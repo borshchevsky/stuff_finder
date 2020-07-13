b21b426332feb7b2bde7c24c5c2ba4a183594805
Pavel <3410914@gmail.com>
2020-07-13 23:08:24 +0300

    поправил форму регистрации

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -16,22 +16,24 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('Имя', validators=[DataRequired()],
-                           render_kw={'class': 'form-control', 'placeholder': 'Имя'})
+                           render_kw={'class': 'form-control', 'placeholder': 'Имя', 'style': 'border-radius:0%;'})
     email = StringField('Email', validators=[DataRequired(), Email()],
-                        render_kw={'class': 'form-control', 'placeholder': 'Электронный адрес'})
+                        render_kw={'class': 'form-control', 'placeholder': 'Электронный адрес', 'style': 'border-radius:0%;'})
     password = PasswordField(
         'Пароль', validators=[DataRequired()],
-        render_kw={'class': 'form-control', 'placeholder': 'Пароль'})
+        render_kw={'class': 'form-control', 'placeholder': 'Пароль', 'style': 'border-radius:0%;'})
     password2 = PasswordField(
         'Повторите пароль',
         validators=[DataRequired(), EqualTo('password', message='Пароли должны совпадать')],
-        render_kw={'class': 'form-control', 'placeholder': 'Повторите пароль'})
-    submit = SubmitField('Отправить', render_kw={'class': 'btn btn-primary'})
+        render_kw={'class': 'form-control', 'placeholder': 'Повторите пароль', 'style': 'border-radius:0%;'})
+    submit = SubmitField('Отправить', render_kw={'class': 'login100-form-btn'})
 
     def validate_username(self, username):
         if User.query.filter_by(username=username.data).count():
-            raise ValidationError('Пользователь с таким именем уже существует.')
+            raise ValidationError('Пользователь с таким именем уже существует')
 
     def validate_email(self, email):
         if User.query.filter_by(email=email.data).count():
             raise ValidationError('Пользователь с таким адресом почты уже существует.')
+        if email.data == '':
+            raise ValidationError('Введите email-адрес')
