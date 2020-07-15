028df3d168035e846a0565657d885d745732b21c
Pavel <3410914@gmail.com>
2020-07-15 10:47:00 +0300

    добавил таски

--- tasks.py
+++ tasks.py
@@ -10,9 +10,11 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 
 @celery_app.on_after_configure.connect
 def setup_tasks(sender, **kwargs):
-    sender.add_periodic_task(crontab(hour='*/1'), update_prices_megafon.s())
+    sender.add_periodic_task(crontab(hour='*/5'), update_prices_megafon.s())
     sender.add_periodic_task(crontab(hour='*/5'), update_prices_eldorado.s())
     sender.add_periodic_task(crontab(hour='*/5'), update_prices_techport.s())
+    sender.add_periodic_task(crontab(hour='*/5'), update_prices_citilink.s())
+    sender.add_periodic_task(crontab(hour='*/5'), update_prices_mts.s())
 
 
 @celery_app.task
@@ -31,3 +33,15 @@ def update_prices_eldorado():
 def update_prices_techport():
     with flask_app.app_context():
         price_parsers.TechportParser().update_db()
+
+
+@celery_app.task
+def update_prices_citilink():
+    with flask_app.app_context():
+        price_parsers.CitilinkParser().update_db()
+
+
+@celery_app.task
+def update_prices_mts():
+    with flask_app.app_context():
+        price_parsers.MtsParser().update_db()
